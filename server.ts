7d146209c6173f335fdc6ea2d94bae7dda777c16
aronboliveira <aron.b.96@gmail.com>
2026-03-30 11:31:24 -0300

    replaced server.ts angular AngularAppEngine through getContext from netlify

--- server.ts
+++ server.ts
@@ -3,7 +3,9 @@ import { getContext } from '@netlify/angular-runtime/context.mjs';
 
 const angularAppEngine = new AngularAppEngine();
 
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
+export async function netlifyAppEngineHandler(
+  request: Request,
+): Promise<Response> {
   const context = getContext();
 
   const result = await angularAppEngine.handle(request, context);
