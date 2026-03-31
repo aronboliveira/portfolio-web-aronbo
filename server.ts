23623d4320511768adfe19ebd2232cf4f4bb77de
aronboliveira <aron.b.96@gmail.com>
2026-03-31 11:12:04 -0300

    chore: patch for vulnerability on picomatch

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { CommonEngine } from '@angular/ssr/node';
 import { render } from '@netlify/angular-runtime/common-engine.mjs';
 
 const commonEngine = new CommonEngine();
+
 export async function netlifyCommonEngineHandler(
   request: Request,
   context: any,
